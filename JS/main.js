9cd7b3cb790ac241c603c3558f46666c27792dc1
Moringa School <karanjasam210@gmail.com>
2022-03-24 07:04:10 +0300

    ci: add funcrtionality to hide an influencer's profile when another profile is clicked

--- JS/main.js
+++ JS/main.js
@@ -5,31 +5,31 @@ $(".lifestyle").click(function(){
 })
 
 $(".one").click(function(){
-    
+    $(".profile").hide();
     $(".profile1").toggle();
 })
 
 $(".two").click(function(){
-   
+    $(".profile").hide();
     $(".profile2").toggle();
 })
 
 $(".three").click(function(){
-   
+    $(".profile").hide();
     $(".profile3").toggle();
 })
 
 $(".four").click(function(){
-   
+    $(".profile").hide();
     $(".profile4").toggle();
 })
 
 $(".five").click(function(){
-   
+    $(".profile").hide();
     $(".profile5").toggle();
 })
 
 $(".six").click(function(){
-    $("#influencers").show();
+    $(".profile").hide();
     $(".profile6").toggle();
 })
